794d4c6e37e83ef3d174c1606e25890cce87cd70
bowen <bowen@yige-group.com>
2022-06-14 13:24:15 +0800

    feat: husky和lint-stage测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from "react"
-import * as dayjs from "dayjs"
+import React from "react";
+import * as dayjs from "dayjs";
 export const App = () => {
   const execute = () => {
     import(/*webpackChunkName:'test'*/ "./utils/index").then(
       ({ formatDogWang }) => {
-        formatDogWang()
+        formatDogWang();
       }
-    )
-  }
+    );
+  };
   return (
     <div class="rect" onClick={execute}>
       {dayjs().format("YYYY-MM-DD")}
     </div>
-  )
-}
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from "react"
+import React from "react";
 
-import ReactDom from "react-dom"
+import ReactDom from "react-dom";
 
-import { App } from "./App"
+import { App } from "./App";
 
-import "@/style/index.less"
+import "@/style/index.less";
 
-import "@/style/rect.less"
+import "@/style/rect.less";
 
-ReactDom.render(<App></App>, document.querySelector("#app"))
+ReactDom.render(<App></App>, document.querySelector("#app"));

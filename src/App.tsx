0237bfe02679fdc7dd1ae728de00e7ed9169bf09
dogwang <dogwang3284304056@gmail.com>
2022-08-14 21:56:02 +0800

    feat: 删除无关代码

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,8 @@ import { Button } from "ui";
 import React from "react";
 
 export const App = () => {
-  const execute = () => {
-    import(/*webpackChunkName:'test'*/ "./utils/index").then(
-      ({ formatDogWang }) => {
-        formatDogWang();
-      }
-    );
-  };
   return (
-    <div className="rect" onClick={execute}>
+    <div className="rect">
       <Button text="按钮"></Button>
     </div>
   );
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,1 @@
-export const formatDogWang = () => {
-  console.log("formatDogWang")
-}
+

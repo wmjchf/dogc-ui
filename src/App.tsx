cffa7c5d15c15ad7f51ed49dd59b41adcd758ebb
bowen <bowen@yige-group.com>
2023-03-17 12:58:38 +0800

    feat: example引入postcss-px2vw

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { Button } from "ui";
 import "./style/rect.less";
 
 export const App = () => {
-  return <div className="rect"></div>;
+  return (
+    <div className="rect">
+      <Button text="按钮"></Button>
+    </div>
+  );
 };

a32d587ca7a49da655f0e631bb9ca41ba3e7bcd3
bowen <bowen@yige-group.com>
2023-03-18 08:09:44 +0800

    feat: 修改button组件

--- packages/ui/components/button/index.tsx
+++ packages/ui/components/button/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import classNames from "classnames";
 import { ICommonComponentProps } from "@components/type";
 import { Context } from "../config-provider/context";
+
 export type IButton = {
   prefixCls?: string;
   children?: React.ReactNode | string;
 } & ICommonComponentProps;
+
 const Button: React.FC<IButton> = (props) => {
   const { prefixCls: customPrefixCls, children, className, style } = props;
   const { getPrefixCls } = React.useContext(Context);
--- packages/ui/components/index.ts
+++ packages/ui/components/index.ts
@@ -1,3 +1,4 @@
+export type { IButton } from "./button/index";
 export { default as Button } from "./button/index";
 
 export { default as ConfigProvider } from "./config-provider/index";

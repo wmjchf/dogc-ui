81b1c852a58ef05df69e770819f9ca1d20d63f3c
wmjchf <3284304056@qq.com>
2023-03-26 19:20:09 +0800

    feat: 增加popup打开关闭动画和position属性

--- packages/ui/components/popup/index.tsx
+++ packages/ui/components/popup/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import classNames from "classnames";
 import { ICommonComponentProps } from "../type";
@@ -9,6 +9,8 @@ export type IPopupProps = {
   children?: React.ReactNode | string;
   position?: "top" | "right" | "bottom" | "left";
   selectorId?: string;
+  visible?: boolean;
+  onClose?: () => void;
 } & ICommonComponentProps;
 
 type RootNode = HTMLElement | null;
@@ -21,11 +23,42 @@ const Popup: React.FC<IPopupProps> = (props) => {
     style,
     position,
     selectorId,
+    visible = false,
+    onClose,
   } = props;
   const { getPrefixCls } = React.useContext(Context);
   const prefixCls = getPrefixCls("popup", customPrefixCls);
-  const classes = classNames(prefixCls, {});
   const [rootNode, setRootNode] = useState<RootNode>(null);
+  const [_visible, _setVisible] = useState<boolean>(false);
+
+  const classes = classNames(prefixCls, {});
+  const maskClasses = classNames({
+    [`${prefixCls}--mask`]: true,
+    [`${prefixCls}--mask_open`]: _visible,
+  });
+  const containerClasses = classNames({
+    [`${prefixCls}--container`]: true,
+  });
+  const bodyClasses = classNames({
+    [`${prefixCls}--body`]: true,
+    [`${prefixCls}--body_open`]: _visible,
+    [`${prefixCls}--body_center`]: !position,
+    [`${prefixCls}--body_top`]: position === "top",
+    [`${prefixCls}--body_bottom`]: position === "bottom",
+    [`${prefixCls}--body_left`]: position === "left",
+    [`${prefixCls}--body_right`]: position === "right",
+  });
+
+  const divRef = useRef<HTMLDivElement>(null);
+  const bodyRef = useRef<HTMLDivElement>(null);
+  const transitionClose = () => {
+    if (!_visible) {
+      onClose && onClose();
+    }
+    divRef.current?.removeEventListener("transitionend", transitionClose);
+  };
+  const isVer = position === "bottom" || position === "top";
+
   useEffect(() => {
     let rootNode: RootNode = null;
     if (selectorId) {
@@ -35,6 +68,86 @@ const Popup: React.FC<IPopupProps> = (props) => {
     }
     setRootNode(rootNode);
   }, [selectorId]);
-  return rootNode && createPortal(<div className={classes}></div>, rootNode);
+
+  useEffect(() => {
+    _setVisible(visible);
+  }, [visible]);
+
+  useEffect(() => {
+    divRef.current?.addEventListener("transitionend", transitionClose);
+  }, [_visible]);
+  const ani = function (
+    div: HTMLDivElement,
+    target: number,
+    direction: -1 | 1
+  ) {
+    if (!position) return;
+    const positionGap = div.style[position].replace("px", "") || "0";
+
+    const positionNum = parseInt(positionGap);
+    if (positionNum === target) return;
+
+    div.style[position] =
+      direction > 0
+        ? `${positionNum + 15 > target ? target : positionNum + 15}px`
+        : `${positionNum - 15 < target ? target : positionNum - 15}px`;
+    requestAnimationFrame(() => {
+      ani(div, target, direction);
+    });
+  };
+  useEffect(() => {
+    if (!bodyRef.current || !_visible || !position) return;
+
+    requestAnimationFrame(function () {
+      ani(bodyRef.current as HTMLDivElement, 0, 1);
+    });
+  }, [_visible]);
+  const _close = () => {
+    _setVisible(false);
+    if (!bodyRef.current || !position) return;
+    requestAnimationFrame(function () {
+      ani(
+        bodyRef.current as HTMLDivElement,
+        -(isVer
+          ? (bodyRef.current?.clientHeight as number)
+          : (bodyRef.current?.clientWidth as number)),
+        -1
+      );
+    });
+  };
+  if (!visible) return <></>;
+
+  return (
+    rootNode &&
+    createPortal(
+      <div className={classes}>
+        <div className={maskClasses} ref={divRef}></div>
+        <div
+          className={containerClasses}
+          onClick={() => {
+            _close();
+          }}
+        >
+          <div
+            className={bodyClasses}
+            ref={bodyRef}
+            style={{
+              [position || ""]: `-${
+                isVer
+                  ? bodyRef.current?.clientHeight || 0
+                  : bodyRef.current?.clientWidth || 0
+              }px`,
+            }}
+            onClick={(event) => {
+              event.stopPropagation();
+            }}
+          >
+            {children}
+          </div>
+        </div>
+      </div>,
+      rootNode
+    )
+  );
 };
 export default Popup;
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React from "react";
-import { Popup } from "ui";
+import React, { useState } from "react";
+import { Popup, Button } from "ui";
 import "./style/rect.less";
 
 export const App = () => {
+  const [visible, setVisisble] = useState(false);
   return (
     <div className="rect" id="rect">
-      <Popup selectorId="rect"></Popup>
+      <Popup
+        selectorId="rect"
+        visible={visible}
+        onClose={() => {
+          setVisisble(false);
+        }}
+        position="bottom"
+      >
+        <div style={{ height: "200px" }}></div>
+      </Popup>
+      <Button
+        type="primary"
+        onClick={() => {
+          setVisisble(true);
+        }}
+        style={{ margin: "10px" }}
+      >
+        打开Popup
+      </Button>
     </div>
   );
 };

ced205a979930f2fed2343271a40adedb7e206dc
wmjchf <3284304056@qq.com>
2023-09-28 10:59:51 +0800

    feat: 隐藏button和image组件

--- packages/ui/components/index.ts
+++ packages/ui/components/index.ts
@@ -1,13 +1,7 @@
 import "ui-theme-default/global.css";
 
-export { default as Button } from "./button";
-export type { IButtonProps } from "./button";
-
 export { default as Popup } from "./popup";
 export type { IPopupProps } from "./popup";
-
-export { default as Image } from "./image";
-export type { IImageProps } from "./image";
 
 export { default as Ellipsis } from "./ellipsis";
 export type { IEllipsisProps } from "./ellipsis";
